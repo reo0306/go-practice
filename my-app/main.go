3870bb7a053af6774d266c94bb26bff8a63a3222
reo0306 <reo19840306@gmail.com>
2026-04-05 06:58:54 +0900

    update main

--- my-app/main.go
+++ my-app/main.go
@@ -1,7 +1,64 @@
 package main
 
 import "fmt"
+import "sort"
+
+type MyString string
+
+type User struct {
+	Name string
+	Age int
+}
+
+type Value int
+
+func showName(user *User) {
+	fmt.Println(user.Name)
+}
+
+func (v Value) Add(n Value) Value {
+	return v + n
+}
 
 func main() {
-	fmt.Println("こんにちは 世界")
+	m := make(map[string]int)
+	m["John"] = 23
+	m["Bob"] = 21
+	m["Mark"] = 22
+	fmt.Println(m)
+
+	for k, v := range m {
+		fmt.Printf("key: %v, value: %v\n", k, v)
+	}
+
+	keys := []string{}
+	for k := range m {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		fmt.Printf("key: %v, value: %v", k, m[k])
+	}
+
+	var a MyString
+	a = "foo"
+	fmt.Println(a)
+
+	user := User {
+		Name: "Bob",
+		Age: 18,
+	}
+
+	showName(&user)
+
+	v := Value(1)
+	v = v.Add(2)
+	fmt.Println(v)
+
+	s := 1
+	p := &s
+	*p = 2
+	fmt.Println(s)
+
+
 }
