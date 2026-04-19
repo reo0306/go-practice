450b3e24d12c345b5f14efcc9cec7e93e7fd2b64
reo0306 <reo19840306@gmail.com>
2026-04-19 14:54:20 +0900

    update cmd

--- aozora-search/cmd/aozora-collector/main.go
+++ aozora-search/cmd/aozora-collector/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"bytes"
+	"io/ioutil"
+        "database/sql"
 	"fmt"
 	"log"
+	"errors"
 	"regexp"
 	"strings"
 	"path"
 	"net/url"
+	"net/http"
 	"archive/zip"
-	"golang.org/x/text/encoding/japanese"
 
+	"golang.org/x/text/encoding/japanese"
 	"github.com/PuerkitoBio/goquery"
+        "github.com/ikawaha/kagome-dict/ipa"
+        "github.com/ikawaha/kagome/v2/tokenizer"
+        _ "github.com/mattn/go-sqlite3"
 )
 
 type Entry struct {
@@ -115,7 +123,7 @@ func extractText(zipURL string) (string, error) {
 			if err != nil {
 				return "", err
 			}
-			b, err := japanese.ShiftJIS.NewDecoder().Bytes(b)
+			b, err = japanese.ShiftJIS.NewDecoder().Bytes(b)
 			if err != nil {
 				return "", err
 			}
@@ -125,21 +133,103 @@ func extractText(zipURL string) (string, error) {
 	return "", errors.New("contens not found")
 }
 
-func main() {
-	listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+func setupDB(dsn string) (*sql.DB, error) {
+        db, err := sql.Open("sqlite3", dsn)
+        if err != nil {
+                return nil, err
+        }
 
-	entries, err := findEntries(listURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, entry := range entries {
-		content, err := extractText(entry.ZipURL)
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		fmt.Println(entry.SiteURL)
-		fmt.Println(content)
-	}
+        queries := []string{
+                `CREATE TABLE IF NOT EXISTS authors(author_id TEXT, author TEXT, PRIMARY KEY(author_id))`,
+                `CREATE TABLE IF NOT EXISTS contents(author_id TEXT, title_id TEXT, title TEXT, content TEXT, PRIMARY KEY(author_id, title_id))`,
+                `CREATE VIRTUAL TABLE IF NOT EXISTS contents_fts USING fts4(words)`,
+        }
+
+        for _, query := range queries {
+                _, err := db.Exec(query)
+                if err != nil {
+                        log.Fatal(err)
+                }
+        }
+
+
+        return db, nil
+}
+
+func addEntry(db *sql.DB, entry *Entry, content string) error {
+        _ ,err := db.Exec(`
+                REPLACE INTO authors(author_id, author) values(?, ?)`,
+                entry.AuthorID,
+                entry.Author,
+        )
+        if err != nil {
+                return err
+        }
+
+        res, err := db.Exec(`
+                REPLACE INTO contents(author_id, title_id, title, content) values(?, ?, ?, ?)`,
+                entry.AuthorID,
+                entry.TitleID,
+                entry.Title,
+                content,
+        )
+        if err != nil {
+                return err
+        }
+        docID, err := res.LastInsertId()
+        if err != nil {
+                return err
+        }
+
+        t, err := tokenizer.New(ipa.Dict(), tokenizer.OmitBosEos())
+        if err != nil {
+                return err
+        }
+
+        seq := t.Wakati(content)
+        _, err = db.Exec(`
+                REPLACE INTO contents_fts(docid, words) values(?, ?)`,
+                docID,
+                strings.Join(seq, " "),
+        )
+        if err != nil {
+                return err
+        }
+
+        return nil
+}
+
+func main() {
+	db, err := setupDB("database.sqlite")
+        if err != nil {
+                log.Fatal(err)
+        }
+        defer db.Close()
+
+
+        listURL := "https://www.aozora.gr.jp/index_pages/person879.html"
+
+        entries, err := findEntries(listURL)
+        if err != nil {
+                log.Fatal(err)
+        }
+        log.Printf("found %d entries", len(entries))
+
+        for _, entry := range entries {
+                log.Printf("adding %+v\n", entry)
+                content, err := extractText(entry.ZipURL)
+
+                if err != nil {
+                        log.Println(err)
+                        continue
+                }
+
+                err = addEntry(db, &entry, content)
+                if err != nil {
+                        log.Println(err)
+                        continue
+                }
+        }
+
 }
 
